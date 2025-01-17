4a21dfbac234fefefeb6b799b0eb3274f3441487
Forsythe <forsythe.dev@gmail.com>
2025-01-17 08:40:31 +0400

    Add alt getBlogPost function to getting msword document's html

--- src/utils/get-post-content.ts
+++ src/utils/get-post-content.ts
@@ -1,6 +1,9 @@
 import fs from 'fs'
+import path from 'path'
+import slugify from 'slugify'
 import matter from 'gray-matter'
-import { transliterate, slugify } from 'transliteration'
+
+import { readDocFile } from '@/utils/read-doc-file'
 
 export function getPostContent(slug: string, path?: string) {
 	const baseUrl = path ?? 'public/blog'
@@ -17,14 +20,39 @@ export function getPostContent(slug: string, path?: string) {
 			const content = fs.readFileSync(filePath, 'utf-8')
 			const compile = matter(content)
 
-			if (
-				compile.content &&
-				slugify(transliterate(compile.data.title)) === slug
-			) {
+			if (compile.content && slugify(compile.data.title) === slug) {
 				return compile
 			}
 		}
 
 		continue
+	}
+}
+
+export async function getBlogPost(slug: string) {
+	const fileDir = 'public/blog'
+
+	const isDirExist = fs.existsSync(fileDir)
+
+	if (!isDirExist) await fs.promises.mkdir(fileDir)
+
+	const files = await fs.promises.readdir(fileDir)
+
+	const file = files.find((file) => {
+		const originalName = path.basename(file, path.extname(file)).toLowerCase()
+		return slug === slugify(originalName).toLowerCase()
+	})
+
+	if (!file) return null
+
+	const filePath = path.join(fileDir, file)
+
+	const html = await readDocFile(filePath)
+	const originalName = path.basename(file, path.extname(file))
+
+	return {
+		slug: slugify(originalName).toLowerCase(),
+		title: originalName,
+		content: matter(html)
 	}
 }
