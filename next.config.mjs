da63dc026ae0cbc696419a75119ed91ef911c314
4Forsythe <forsythe.dev@gmail.com>
2025-02-13 13:38:11 +0400

    Remove api rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,6 @@ import createMDX from '@next/mdx'
 import withPlaiceholder from '@plaiceholder/next'
 
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: `${process.env.API_BASE_URL}/:path*`
-			}
-		]
-	},
 	images: {
 		remotePatterns: [
 			{
