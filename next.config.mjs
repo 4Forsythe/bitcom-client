8e5fa7f9d358d691f8f3767b7ffa185d022e7053
4Forsythe <forsythe.dev@gmail.com>
2025-02-17 12:31:30 +0400

    Upd: add next.config.mjs rewrites for dev env mode

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ import createMDX from '@next/mdx'
 import withPlaiceholder from '@plaiceholder/next'
 
 const nextConfig = {
+	async rewrites() {
+		const isDevMode = process.env.NODE_ENV !== 'production'
+		const API_BASE_URL = process.env.API_BASE_URL
+		const API_GLOBAL_PREFIX = process.env.NEXT_PUBLIC_API_GLOBAL_PREFIX
+
+		if (isDevMode) {
+			return [
+				{
+					source: `${API_GLOBAL_PREFIX}/:path*`,
+					destination: `${API_BASE_URL}/:path*`
+				}
+			]
+		}
+	},
 	images: {
 		remotePatterns: [
 			{
