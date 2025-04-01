bfafe9c2974eef842c1daa20b433686cdf4f3163
4Forsythe <forsythe.dev@gmail.com>
2025-04-01 17:54:24 +0400

    Upd: remake thanks component

--- src/components/Thanks/index.tsx
+++ src/components/Thanks/index.tsx
@@ -1,49 +1,47 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 
 import clsx from 'clsx'
+
 import { Button } from '@/components'
 import { ROUTE } from '@/config/routes.config'
 import { PHONE, SECOND_PHONE } from '@/constants'
 
-import type { UserType } from '@/types/user.types'
 import type { OrderType } from '@/types/order.types'
 
 import styles from './thanks.module.scss'
 
 interface IThanks {
-	user: UserType
 	order: OrderType
 }
 
-export const Thanks: React.FC<IThanks> = ({ user, order }) => {
+export const Thanks: React.FC<IThanks> = ({ order }) => {
 	return (
 		<div className={styles.container}>
 			<div className={clsx(styles.block, 'animate-bounce')}>
-				<h5 className={styles.heading}>Спасибо за заказ!</h5>
+				<div className={styles.heading}>
+					<h5 className={styles.title}>Ваш заказ —</h5>
+					<span className={styles.orderHash}>
+						{new Date(order.createdAt).getTime().toString().slice(-8)}
+					</span>
+				</div>
 
 				<span className={styles.caption}>
-					<Image
-						width={20}
-						height={20}
-						src='/icons/Success.svg'
-						alt='Успешная операция'
-					/>
-					Ваш заказ был оформлен на <b>{user.email}</b>
+					Обязательно запишите или сфотографируйте номер, у вас могут спросить
+					его при получении заказа.
 				</span>
-
 				<div className={styles.divider} />
+				<p className={styles.paragraph}>
+					Благодарим вас за предварительное оформление заказа,
+					<br />В ближайшее время с вами свяжутся наши специалисты из отдела
+					продаж.
+				</p>
+				<div className={styles.divider} />
+				<p className={styles.paragraph}>
+					При наличии дополнительных вопросов, просьба позвонить нам или
+					уточнить детали в Telegram/Whatsapp:
+				</p>
 
-				<p className={styles.paragraph}>
-					Благодарим вас за{' '}
-					{+order.total > 0
-						? `покупку на сумму ${order.total} ₽`
-						: 'предварительную покупку'}
-				</p>
-				<p className={styles.paragraph}>
-					Детали можно посмотреть в статусе или уточнить у наших специалистов:
-				</p>
 				<ul className={styles.list}>
 					<li className={styles.listItem}>{PHONE}</li>
 					<li className={styles.listItem}>{SECOND_PHONE}</li>
@@ -51,10 +49,7 @@ export const Thanks: React.FC<IThanks> = ({ user, order }) => {
 
 				<div className={styles.controls}>
 					<Link href={ROUTE.HOME}>
-						<Button variant='outlined'>На главную</Button>
-					</Link>
-					<Link href={ROUTE.ORDERLIST}>
-						<Button>В заказы</Button>
+						<Button variant='outlined'>Вернутсья на главную</Button>
 					</Link>
 				</div>
 			</div>
