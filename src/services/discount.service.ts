5a315aaa824fdb7aa307161edd2f6ed6d39d6c64
4Forsythe <forsythe.dev@gmail.com>
2025-08-20 14:27:25 +0400

    Del: remove discount types

--- src/services/discount.service.ts
+++ src/services/discount.service.ts
@@ -1,19 +1,14 @@
 import { api } from '@/api/interceptors/api-instance'
-import type {
-	DiscountType,
-	DiscountsType,
-	DiscountParams
-} from '@/types/discount.types'
 
 class DiscountService {
 	private endpoint = '/discounts'
 
-	async getAll(params?: DiscountParams): Promise<DiscountsType> {
-		const response = await api.get(this.endpoint, { params })
+	async getAll() {
+		const response = await api.get(this.endpoint)
 		return response.data
 	}
 
-	async getOne(id: string): Promise<DiscountType> {
+	async getOne(id: string) {
 		const response = await api.get(`${this.endpoint}/${id}`)
 		return response.data
 	}
