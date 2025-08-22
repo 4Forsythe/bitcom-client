77f5263a13a275e6da2ee03ed1b4e6b921076e00
4Forsythe <forsythe.dev@gmail.com>
2025-08-22 14:19:15 +0400

    Add discount service

--- src/services/discount.service.ts
+++ src/services/discount.service.ts
@@ -2,6 +2,8 @@ import { api, apiWithHeaders } from '@/api/interceptors/api-instance'
 
 import type {
 	DiscountType,
+	DiscountsType,
+	DiscountParamsType,
 	CreateDiscountType,
 	UpdateDiscountType
 } from '@/types/discount.types'
@@ -16,6 +18,21 @@ class DiscountService {
 
 	async update(id: string, data: UpdateDiscountType): Promise<DiscountType> {
 		const response = await apiWithHeaders.patch(`${this.endpoint}/${id}`, data)
+		return response.data
+	}
+
+	async getAll(params?: DiscountParamsType): Promise<DiscountsType> {
+		const response = await api.get(this.endpoint, { params })
+		return response.data
+	}
+
+	async getActual(params?: DiscountParamsType): Promise<DiscountsType> {
+		const response = await api.get(`${this.endpoint}/actual`, { params })
+		return response.data
+	}
+
+	async getOne(id: string): Promise<DiscountType> {
+		const response = await api.get(`${this.endpoint}/${id}`)
 		return response.data
 	}
 }
