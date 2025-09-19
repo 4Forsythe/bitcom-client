ba99456a78a5acc3e607c086efe33d61d4f0319e
4Forsythe <forsythe.dev@gmail.com>
2025-09-19 17:29:12 +0400

    Upd: add dynamic prices link

--- src/app/(root)/(page)/prices/page.tsx
+++ src/app/(root)/(page)/prices/page.tsx
@@ -4,8 +4,6 @@ import { ROUTE } from '@/config/routes.config'
 
 import { Prices } from './prices'
 import { Breadcrumb } from '@/components'
-import { getPrices } from '@/utils/get-prices'
-import { notFound } from 'next/navigation'
 
 export const metadata: Metadata = {
 	title: 'Прайс-листы',
@@ -14,19 +12,13 @@ export const metadata: Metadata = {
 }
 
 export default async function PricesPage() {
-	const data = await getPrices()
-
-	if (!data || !data.length) {
-		return notFound()
-	}
-
 	return (
 		<>
 			<Breadcrumb
 				value='Прайс-листы'
 				items={[{ href: ROUTE.HOME, value: 'Главная' }]}
 			/>
-			<Prices items={data} />
+			<Prices />
 		</>
 	)
 }
--- src/app/(root)/(page)/prices/prices.tsx
+++ src/app/(root)/(page)/prices/prices.tsx
@@ -1,36 +1,36 @@
-'use client'
-
 import React from 'react'
 import Link from 'next/link'
-
 import { File } from 'lucide-react'
+
+import { InfoBlock } from '@/components'
 
 import styles from './prices.module.scss'
 
-interface Props {
-	items: string[]
-}
+const API_BASE_URL = String(process.env.API_BASE_URL)
 
-export const Prices: React.FC<Props> = ({ items }) => {
+export const Prices: React.FC = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.section}>
 				<h5 className={styles.title}>Скачать прайс-листы</h5>
 				<div className={styles.list}>
-					{items.map((file, index) => (
-						<Link
-							key={index}
-							className={styles.chip}
-							href={`/assets/prices/${file}`}
-							target='_blank'
-						>
-							<div className={styles.icon}>
-								<File size={16} />
-							</div>
-							{file}
-						</Link>
-					))}
+					<Link
+						className={styles.chip}
+						href={`${API_BASE_URL}/product/export`}
+						target='_blank'
+					>
+						<div className={styles.icon}>
+							<File size={18} />
+						</div>
+						Все товары
+					</Link>
 				</div>
+				<InfoBlock
+					className={styles.infoBlock}
+					variant='knowledge'
+				>
+					Данные могут обновляться с задержкой до 1 ч. реального времени.
+				</InfoBlock>
 			</div>
 		</div>
 	)
